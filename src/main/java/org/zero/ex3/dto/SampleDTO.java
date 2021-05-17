c5873f9fcbfe6ae08f832c5e34085c0f1a45a51c
sasca37 <sasca37@naver.com>
2021-05-17 19:28:02 +0900

    sda

---
+++ src/main/java/org/zero/ex3/dto/SampleDTO.java
@@ -0,0 +1,15 @@
+package org.zero.ex3.dto;
+
+import java.time.LocalDateTime;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder(toBuilder = true)
+public class SampleDTO {
+	private Long sno;
+	private String first;
+	private String last;
+	private LocalDateTime regTime;
+}
